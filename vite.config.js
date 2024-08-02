d4a1366918188b099d969d55c78e4a58d2ba6d7d
Daniel Taylor <danieltaylor@Daniels-MacBook-Pro.local>
2024-08-02 21:29:01 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nc-marketplace/", // Correct base URL
+  build: {
+    sourcemap: true, 
+  },
+  base: '/nc-marketplace/',
 });
